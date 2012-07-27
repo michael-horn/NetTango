5449f0af80396a0286951f086f22ec3396cf5ef9
Michael Horn <michael-horn@northwestern.edu>
2012-07-27 01:01:15 -0500

    Added fullscreen / partscreen buttons

--- java/ntango/Toolbar.java
+++ java/ntango/Toolbar.java
@@ -26,7 +26,10 @@ public class Toolbar extends Touchable {
    protected Button playButton;
    protected Button pauseButton;
    protected Button scrubButton;
+   protected Button fullButton;
+   protected Button partButton;
    protected int scrubMax;
+   protected boolean fullscreen = false;
 
    
    public Toolbar(Model model) {
@@ -86,11 +89,20 @@ public class Toolbar extends Touchable {
       button = new Button(bx, by, bw, bh, "restart");
       button.setImage(Palette.createImage("/images/restart.png"));
       buttons.add(button);
-
-      //button = new Button(w - bw - 10, by, bw, bh, "fullscreen");
-      //button.setImage(Palette.createImage("/images/fullscreen.png"));
-      //buttons.add(button);
       
+      bx = w - 10 - bw;
+      button = new Button(bx, by, bw, bh, "fullscreen");
+      button.setImage(Palette.createImage("/images/fullscreen.png"));
+      buttons.add(button);
+      fullButton = button;
+      
+      button = new Button(bx, by, bw, bh, "partscreen");
+      button.setImage(Palette.createImage("/images/partscreen.png"));
+      buttons.add(button);
+      button.setVisible(false);
+      button.setEnabled(false);
+      partButton = button;
+
       bw = 250;
       bh = 10;
       bx = w/2 - bw/2;
@@ -101,6 +113,22 @@ public class Toolbar extends Touchable {
       buttons.add(button);
       scrubButton = button;
    }
+   
+   
+   public boolean isPlaying() {
+      return this.pstate > 0;
+   }
+   
+   
+   public boolean isPaused() {
+      return this.pstate == 0;
+   }
+   
+   
+   public boolean isFullscreen() {
+      return this.fullscreen;
+   }
+   
 
    public void draw(Graphics2D g) {
       
@@ -123,7 +151,7 @@ public class Toolbar extends Touchable {
       //---------------------------------------------
       g.setFont(Palette.FONT_H1);
       if (pstate > 1) {
-         g.drawString("x" + pstate, w/2 + 135, h - 10);
+         g.drawString("x" + pstate, w/2 - 160, h - 10);
       }
       else if (pstate < -1) {
          g.drawString("x" + (pstate * -1), w/2 - 160, h - 10);
@@ -135,7 +163,7 @@ public class Toolbar extends Touchable {
       g.setFont(Palette.FONT_BODY);
       String s = "tick: " + model.getPlayHead();
       int fw = g.getFontMetrics().stringWidth(s);
-      g.drawString(s, w - fw - 6, 25);
+      g.drawString(s, w - fw - 10, h - 15);
       
       //---------------------------------------------
       // Draw the scrub bar
@@ -258,13 +286,13 @@ public class Toolbar extends Touchable {
    public void onHover() { }
 
    public void onClick(Button button) {
+      Main app = Main.instance;
       if ("play".equals(button.getAction())) {
          this.pstate = 1;
       } else if ("pause".equals(button.getAction())) {
          this.pstate = 0;
       } else if ("restart".equals(button.getAction())) {
          model.setup();
-         Main app = Main.instance;
          app.layout(app.getWidth(), app.getHeight());
          this.pstate = 0;
       } else if ("fastforward".equals(button.getAction())) {
@@ -285,7 +313,23 @@ public class Toolbar extends Touchable {
       } else if ("stepback".equals(button.getAction())) {
          pstate = 0;
          movePlayHead(-1);
+      } else if ("fullscreen".equals(button.getAction())) {
+         fullButton.setVisible(false);
+         fullButton.setEnabled(false);
+         partButton.setVisible(true);
+         partButton.setEnabled(true);
+         this.fullscreen = true;
+         app.enterFullscreen();
+         
+      } else if ("partscreen".equals(button.getAction())) {
+         fullButton.setVisible(true);
+         fullButton.setEnabled(true);
+         partButton.setVisible(false);
+         partButton.setEnabled(false);
+         this.fullscreen = false;
+         app.exitFullscreen();
       }
+      
       
       playButton.setEnabled(pstate == 0);
       playButton.setVisible(pstate == 0);
